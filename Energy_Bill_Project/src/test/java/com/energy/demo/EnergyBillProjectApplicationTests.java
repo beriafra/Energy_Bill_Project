8b96c454820572cb109a2fdb87b2ad006acb5cb4
Afra Beria Baki <afraberia5839@gmail.com>
2023-03-03 00:30:42 +0200

    Project creation and first steps

---
+++ Energy_Bill_Project/src/test/java/com/energy/demo/EnergyBillProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.energy.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnergyBillProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
